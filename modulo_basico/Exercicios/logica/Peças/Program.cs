0ce9fca37c0c2a8c68aacf6d2f24fe735184d038
Rodrigo Tomaz <rodrigo_souza2013@yahoo.com.br>
2020-11-25 20:20:26 -0300

    Criado realizado exercicio de peças

--- modulo_basico/Exercicios/logica/Peças/Program.cs
+++ modulo_basico/Exercicios/logica/Peças/Program.cs
@@ -6,7 +6,19 @@ namespace Peças
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double valorTotal = 0;
+            for (int i = 0; i < 2; i++){
+                Console.WriteLine("Digite o código:");
+                int codigo = Console.Read();
+                Console.WriteLine("Digite a quantidade:");
+                int qtde = Console.Read();
+                Console.WriteLine("Digite o valor:");
+                double valor = double.Parse(Console.ReadLine());
+
+                valorTotal += valor;
+            }
+
+            Console.WriteLine("Valor a pagar:"+valorTotal);
         }
     }
 }
