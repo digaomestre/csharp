fcd2fdcaaf3d112976c3774347c5e747b95e2703
Rodrigo Tomaz <rodrigo_souza2013@yahoo.com.br>
2020-11-26 08:16:08 -0300

    Realizado o exercicio de Primeiro Programa e Criado o Exercicio soma

---
+++ Introduçao/l_soma/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace l_soma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
