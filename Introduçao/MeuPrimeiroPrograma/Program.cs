1cf540d2f8b1a76df590c022c16873347a020510
Rodrigo Tomaz <rodrigo_souza2013@yahoo.com.br>
2020-11-26 08:17:11 -0300

    Primeriro programa e soma

--- Introduçao/MeuPrimeiroPrograma/Program.cs
+++ Introduçao/MeuPrimeiroPrograma/Program.cs
@@ -6,7 +6,7 @@ namespace MeuPrimeiroPrograma
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ola mundo!");
         }
     }
 }
